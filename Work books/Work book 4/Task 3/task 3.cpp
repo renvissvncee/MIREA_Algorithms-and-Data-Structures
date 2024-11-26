97929040f831ae3c7f04f727e4ba9b3aedd8268f
Artem Druzhinin <tyomafrutis@mail.ru>
2024-11-26 12:08:41 +0300

    4.3

--- Work books/Work book 4/Task 3/task 3.cpp
+++ Work books/Work book 4/Task 3/task 3.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+double func(double x) {
+    return (0.25*x*x*x) + x - 2;
+}
+
+double f(double L, double R, double E) {
+    if (R-L < E) {
+        return (R+L)/2;
+    }
+    if ((func((R+L)/2) * func(R)) < 0) {
+        return f((R+L)/2, R, E);
+    }
+    else {
+        return f(L, (R+L)/2, E);
+    }
+}
+
 int main() {
-    
+    std::cout << f(0, 3, 0.001);
     return 0;
 }
