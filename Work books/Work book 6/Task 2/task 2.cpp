17c2ff471d8f46108446c284073f8d00eb135668
Artem Druzhinin <tyomafrutis@mail.ru>
2024-12-07 09:32:45 +0300

    6.2

---
+++ Work books/Work book 6/Task 2/task 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    int len = rand() % 10000;
+    int count = 0, sum = 0;
+    int Arr[len];
+
+    for (int i = 0; i < len; i++) {
+        Arr[i] = rand();
+    }
+
+    for (int i = 0; i < len; i++) {
+        if (Arr[i] % 5 == 0 && Arr[i] % 7 != 0) {
+            count++;
+            sum += Arr[i];
+        }
+    }
+
+    std::cout << "Количество: " << count << "\nСумма: " << sum;
+
+    return 0;
+}
